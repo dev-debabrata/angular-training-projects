f92ed90076c4d2de28616f2c430297e48fa2995d
Debabrata Das <debabratadas711@gmail.com>
2026-03-24 21:37:55 +0530

    add header

--- week-4_user-directory-app/src/app/app.ts
+++ week-4_user-directory-app/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
