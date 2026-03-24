71ec974bab0086cbe488d6319925636212444f85
Debabrata Das <debabratadas711@gmail.com>
2026-03-24 21:47:16 +0530

    add users

--- week-4_user-directory-app/src/app/app.routes.ts
+++ week-4_user-directory-app/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Users } from './components/users/users';
+import { UserDetails } from './components/users/user-details/user-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Users },
+  { path: 'user/:id', component: UserDetails },
+  { path: '**', redirectTo: '' },
+
+  //   {
+  //     path: '',
+  //     loadComponent: () => import('./components/user-lists/user-lists').then((m) => m.UserLists),
+  //   },
+  //   {
+  //     path: 'users/:id',
+  //     loadComponent: () =>
+  //       import('./components/user-lists/user-detail/user-detail').then((m) => m.UserDetail),
+  //   },
+  //   {
+  //     path: 'loader',
+  //     loadComponent: () => import('./components/loader/loader').then((m) => m.Loader),
+  //   },
+  //   { path: '**', redirectTo: '' },
+];
--- week-4_user-directory-app/src/app/components/users/users.ts
+++ week-4_user-directory-app/src/app/components/users/users.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { catchError, of } from 'rxjs';
+import { UserService } from '../../services/user.service';
+import { Loader } from '../loader/loader';
+import { Error } from '../error/error';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [Loader, Error, CommonModule, RouterModule],
   templateUrl: './users.html',
   styleUrl: './users.css',
 })
 export class Users {
+  // constructor(private router: Router) {}
 
+  // viewDetails(id: number) {
+  //   this.router.navigate(['/user', id]);
+  // }
+  private router = inject(Router);
+  private userService = inject(UserService);
+
+  loading = true;
+  error = false;
+
+  users$ = this.userService.getUsers().pipe(
+    catchError(() => {
+      this.error = true;
+      this.loading = false;
+      return of([]);
+    }),
+  );
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+  }
+  viewDetails(id: number) {
+    this.router.navigate(['/user', id]);
+  }
 }
