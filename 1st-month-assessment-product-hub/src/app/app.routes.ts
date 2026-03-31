d6fc4179bca19877dad2a1c022061e99a445ef52
Debabrata Das <debabratadas711@gmail.com>
2026-03-31 22:31:42 +0530

    add loader & error

--- 1st-month-assessment-product-hub/src/app/app.routes.ts
+++ 1st-month-assessment-product-hub/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductList } from './components/product-list/product-list';
+import { ProductDetails } from './components/product-details/product-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ProductList },
+  { path: 'product/:id', component: ProductDetails },
+];
--- 1st-month-assessment-product-hub/src/app/app.ts
+++ 1st-month-assessment-product-hub/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
