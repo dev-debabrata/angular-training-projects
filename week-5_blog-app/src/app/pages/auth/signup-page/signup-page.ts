ebacf8d48b96556a1d21d56edc5a9f79aabc722b
Debabrata Das <debabratadas711@gmail.com>
2026-04-04 21:31:21 +0530

    add new

---
+++ week-5_blog-app/src/app/pages/auth/signup-page/signup-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup-page.html',
+  styleUrl: './signup-page.css',
+})
+export class SignupPage {}
