37e1b9f4fe40efabd8a073fcd066943211fef7e9
Debabrata Das <debabratadas711@gmail.com>
2026-04-07 00:13:28 +0530

    add contact

--- week-5_blog-app/src/app/pages/contact/contact.ts
+++ week-5_blog-app/src/app/pages/contact/contact.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
-export class Contact {}
+export class Contact {
+  contact = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  submitted = false;
+
+  submitForm() {
+    console.log('Form Data:', this.contact);
+    this.submitted = true;
+
+    // Reset form after submission
+    this.contact = { name: '', email: '', message: '' };
+  }
+}
